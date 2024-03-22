a7bd3be262c5dc1250677f68f579125546919489
Romulo Tole <tolemaicor@gmail.com>
2024-03-22 01:18:27 -0400

    Agregando todos los models

--- src/models/ambienteModel.py
+++ src/models/ambienteModel.py
@@ -11,8 +11,75 @@ class AmbienteModel():
                 cursor.execute('SELECT cod_ambiente,nombre_amb,descripcion_amb FROM ambiente;')
                 resultset = cursor.fetchall()
                 for row in resultset:
-                    ambientes.append(Ambiente(row[0],row[1]).to_JSON())
+                    ambientes.append(Ambiente(row[0],row[1],row[2]).to_JSON())
             connection.close()
             return ambientes
         except Exception as ex:
             raise Exception(ex)
+
+    @classmethod
+    def get_ambiente(self,id):
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute('SELECT * FROM ambiente WHERE cod_ambiente = %s',(id,))
+                row = cursor.fetchone()
+                ambiente = None
+                if row != None:
+                    ambiente = Ambiente(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9])
+                    ambiente = ambiente.to_JSON()
+                connection.close()
+                return ambiente
+        except Exception as ex:
+            raise Exception(ex)
+    
+    @classmethod
+    def add_ambiente(self,ambiente):
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute('''
+                    INSERT INTO ambiente (nombre_amb,capacidad_amb,ubicacion_amb,descripcion_amb,cod_estado_ambiente,cod_piso,
+                    cod_edificio,cod_facultad,cod_tipo_ambiente) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)''', (
+                    ambiente.nombre_amb,ambiente.capacidad_amb,ambiente.ubicacion_amb,ambiente.descripcion_amb,
+                    ambiente.cod_estado_ambiente,ambiente.cod_piso,ambiente.cod_edificio,ambiente.cod_facultad,
+                    ambiente.cod_tipo_ambiente
+                ))
+                affected_rows = cursor.rowcount
+                connection.commit()
+            connection.close()
+            return affected_rows
+        except Exception as ex:
+            raise Exception(ex)
+
+    @classmethod
+    def delete_ambiente(self,ambiente):
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute('DELETE FROM ambiente WHERE cod_ambiente = %s',(ambiente.cod_ambiente))
+                affected_rows = cursor.rowcount
+                connection.commit()
+            connection.close()
+            return affected_rows
+        except Exception as ex:
+            raise Exception(ex)
+    
+    @classmethod
+    def update_ambiente(self,ambiente):
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute('''UPDATE ambiente SET 
+                nombre_amb = %s, capacidad_amb = %s, ubicacion_amb = %s, descripcion_amb = %s, cod_estado_ambiente = %s, 
+                cod_piso = %s, cod_edificio = %s, cod_facultad = %s, cod_tipo_ambiente = %s WHERE cod_ambiente = %s
+                ''', (ambiente.nombre_amb,ambiente.capacidad_amb,ambiente.ubicacion_amb,ambiente.descripcion_amb,ambiente.cod_estado_ambiente,
+                ambiente.cod_piso,ambiente.cod_edificio,ambiente.cod_facultad,ambiente.cod_tipo_ambiente,ambiente.cod_ambiente
+                ))
+                affected_rows = cursor.rowcount
+                connection.commit()
+            connection.close()
+            return affected_rows
+        except Exception as ex:
+            raise Exception(ex)
+
