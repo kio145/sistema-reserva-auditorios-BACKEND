f2702ccfa82ed430953085aa0522df72793ce81f
Romulo Tole <tolemaicor@gmail.com>
2024-04-27 09:25:02 -0400

    Corrigiendo ruta

--- src/routers/edificacion.py
+++ src/routers/edificacion.py
@@ -6,7 +6,7 @@ main = Blueprint('edificio_blueprint', __name__)
 @main.route('/all')
 def get_edificaciones():
     try:
-        edificaciones = EdificacionModel.get_edificios()
+        edificaciones = EdificacionModel.get_edificaciones()
         return jsonify(edificaciones)
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
